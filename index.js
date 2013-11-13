530ee991b21de9872db3081643f017ee47440c1f
JimmPratt <jimm.pratt@gmail.com>
2013-11-13 17:34:00 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,124 @@
+/* -------------------------------------------------------------------------------------------*/
+// get the position, trap success, and offer up switching to lower accuracy on error
+var options = {
+    maximumAge: 10000,
+    timeout: 10000,
+    enableHighAccuracy: true
+}
+
+navigator.geolocation.watchPosition(
+    successCallback,
+    errorCallback_on_highAccuracy,
+    options
+);
+
+/* -------------------------------------------------------------------------------------------*/
+// callback functions that handles errors,
+// and fall-back on a less accurate position if necessary.
+
+function errorCallback_on_highAccuracy(position) {
+
+    if (error.code == error.TIMEOUT)
+    {
+        // Attempt to get GPS location timed out after 5 seconds,
+        // then try low accuracy location
+        console.log("attempting to get low accuracy location");
+        navigator.geolocation.getCurrentPosition(
+               successCallback,
+               errorCallback_on_lowAccuracy,
+               {maximumAge: 10000, timeout: 10000, enableHighAccuracy: false});
+        return;
+    }
+
+    var msg = "Can't get your location (high accuracy attempt). Error = ";
+    if (error.code == 1)
+        msg += "PERMISSION_DENIED";
+    else if (error.code == 2)
+        msg += "POSITION_UNAVAILABLE";
+    msg += ", msg = " + error.message;
+
+    console.log(msg);
+}
+
+function errorCallback_on_lowAccuracy(position) {
+    var msg = "Can't get your location (low accuracy attempt). Error = ";
+    if (error.code == 1)
+        msg += "PERMISSION_DENIED";
+    else if (error.code == 2)
+        msg += "POSITION_UNAVAILABLE";
+    else if (error.code == 3)
+        msg += "TIMEOUT";
+    msg += ", msg = " + error.message;
+
+    console.log(msg);
+}
+
+/* -------------------------------------------------------------------------------------------*/
+// when we are successful at getting a position, we do all our map creation work inside here
+
+function successCallback(position) {
+    var customerLatitude = position.coords.latitude,
+        customerLongitude = position.coords.longitude,
+        accuracy = position.coords.accuracy,
+        personIcon = L.icon({
+            iconUrl: 'feature/person.png',
+            iconSize:     [24, 24], // size of the icon
+            iconAnchor:   [12, 12], // point of the icon which will correspond to marker's location
+        }),
+        rickshawIcon = L.icon({
+            iconUrl: 'feature/rickshaw.png',
+            iconSize:     [24, 24], // size of the icon
+            iconAnchor:   [12, 12], // point of the icon which will correspond to marker's location
+        });
+    //console.log("Your location is: " + customerLatitude + "," + customerLongitude+" Accuracy="+position.coords.accuracy+"m");
+
+    var map = L.map('map');
+/*     playing with watercolor map.  looks nice, but not very informative
+    var layer = new L.StamenTileLayer('watercolor');
+      map.addLayer(layer); */
+
+    // add an OpenStreetMap tile layer
+    L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+        attribution: 'Accuracy: ' + accuracy + ' meters -  &copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+
+     /* var oms = new OverlappingMarkerSpiderfier(map); */
+
+    // add a marker in the given location, attach some pop-up content to it and open the pop-up
+    //L.marker([customerLatitude, customerLongitude], {icon: greenIcon}).addTo(map).bindPopup('you are here');
+
+    // here is where the user is located
+    L.marker([customerLatitude, customerLongitude], {icon: personIcon})
+      .addTo(map);
+
+    // here are the cycle taxis (later pulled from database)
+    //  var m = L.marker([-37.785, 175.263], {draggable:true}).bindLabel('A sweet static label!', { noHide: true })
+
+
+    L.marker([55.678885020269895, 12.592901587486267]).bindLabel('Jimm is available!', { noHide: true }).addTo(map).showLabel();
+    L.marker([55.68060674857952, 12.587215304374695])
+      .bindPopup('<b>Sebastian</b><br><span style="color:grey">Out to Lunch</span>')
+      .addTo(map);
+    L.marker([55.680203733331545, 12.585026621818542])
+      .bindPopup('<b>George</b><br><span style="color:red">In Service</span>')
+      .addTo(map);
+    L.marker([55.678637756621235, 12.579651474952698])
+      .bindPopup('<b>Lars</b><br><span style="color:red">In Service</span>')
+      .addTo(map);
+    L.marker([55.67867405230058, 12.579709142446518])
+      .bindPopup('<b>Gilli</b><br><span style="color:green">Available</span>')
+      .addTo(map);
+
+    // loop through all the coordinates and find the north-eastern boundaries
+    // and south-western boundaries then scale the map accordingly,
+	 //fitting customer and drivers on the map
+
+	 // replace with array from database
+	 map.fitBounds([[customerLatitude, customerLongitude],
+						 [55.678885020269895, 12.592901587486267],
+						 [55.68060674857952, 12.587215304374695],
+						 [55.680203733331545, 12.585026621818542],
+						 [55.678637756621235, 12.579651474952698],
+						 [55.67867405230058, 12.579709142446518]]
+	 );
+}
---
+++ taximeter/stopwatch.js
@@ -0,0 +1,152 @@
+var $start      = $('#start'),
+	startText   = $start.text(),
+	stopText    = $start.attr('alternate'),
+	$split      = $('#split'),
+	$reset      = $('#reset'),
+	$timer      = $('#timer'),
+	$splits     = $('#splits'),
+	$help       = $('#help'),
+	$helpSwitch = $('#help-switch'),
+	$helpDiv    = $help.find('div'),
+	kronerStart= 40,
+	kronerRate = 4,
+	/*
+	 * I found this code on a few sites and am unsure of the original author.
+	 * If you know please inform me so I can credit them here.
+	 *
+	 * 0 = start time
+	 * 1 = end time
+	 * 2 = state (stopped or counting)
+	 * 3 = total elapsed time in ms
+	 * 4 = timer (interval object)
+	 * 5 = epoch (January 1, 1970)
+	 * 6 = element (not used here, normally stores the DOM element to update with the time)
+	 * 7 = split count
+	 */
+	t = [0, 0, 0, 0, 0, 0, 0, 0],
+	
+	format = function(ms) {
+		var d = new Date(ms + t[5]).toString().replace(/.*(\d{2}:\d{2}:\d{2}).*/, '$1');
+		var x = String(ms % 1000);
+		while (x.length < 3) {
+			x = '0' + x;
+		}
+		d += '.' + x;
+		//return d.substr(0, d.length - 1);
+		return d.substr(0, d.length - 4);
+	},
+	
+	zero = function(num) {
+		if (parseInt(num) < 0) var neg = true;
+		if (Math.abs(parseInt(num)) < 10) {
+			num = '0' + Math.abs(num);
+		}
+		if (neg) num = '-' + num;
+		return num;
+	},
+	
+	startStop = function() {
+		t[t[2]] = (+new Date()).valueOf();
+		t[2] = 1 - t[2];
+		
+		if (t[2] == 0) {
+			clearInterval(t[4]);
+			t[3] += t[1] - t[0];
+			
+			$start.text(startText);
+			
+			t[7]++;
+			$splits.show();
+			dkFare = kronerStart + (format(t[3]).substr(3,2) * kronerRate);
+			usFare = dkFare * 0.171956;
+			euroFare = dkFare * 0.134538;
+			swedenFare = dkFare * 1.22852;
+			norwayFare = dkFare * 1.02541;
+			
+			$('<li>' + dkFare + ' kroner<br>' + Math.floor(swedenFare.toFixed(2)) + ' SEK<br>' + Math.floor(norwayFare.toFixed(2)) + ' NOK<br>' + Math.floor(euroFare.toFixed(2)) + ' EURO<br>' + Math.floor(usFare.toFixed(2)) + ' USD</li>').appendTo($splits).slideDown('fast');
+
+			$splits.find('li').removeClass('first last');
+			$splits.find('li:first').addClass('first').end().find('li:last').addClass('last');
+			
+			t[4] = t[1] = t[0] = 0;
+			
+			display();
+		}
+		else {
+			$start.text(stopText);
+			t[4] = setInterval(display, 43);
+		}
+		
+		return false;
+	},
+
+	reset = function() {
+		if (t[2]) {
+			startStop();
+		}
+		
+		t[4] = t[3] = t[2] = t[1] = t[0] = 0;
+		
+		display();
+		
+		$start.text(startText);
+		$splits.slideUp('fast', function() {
+			$splits.empty();
+		});
+		
+		t[7] = 0;
+		
+		return false;
+	},
+	
+	display = function() {
+		if (t[2]) {
+			t[1] = (new Date()).valueOf();
+		}
+		
+		$timer.text(format(t[3] + t[1] - t[0]));
+	},
+	
+	split = function() {
+		if (t[2] !== 0) {
+			t[7]++;
+			
+			$splits.show();
+			$('<li><span>' + zero(t[7]) + '</span> ' + format(t[3] + t[1] - t[0]) + '</li>').appendTo($splits).slideDown('fast');
+			$splits.find('li').removeClass('first last');
+			$splits.find('li:first').addClass('first').end().find('li:last').addClass('last');
+		}
+		
+		return false;
+	},
+	
+	load = function() {
+		t[5] = new Date(1970, 1, 1, 0, 0, 0, 0).valueOf();
+
+		display();
+	};
+
+$(function() {
+
+	$splits.empty();
+	
+	load();
+	
+	$start.click(startStop);
+	$split.click(split);
+	$reset.click(reset);
+	
+	$helpSwitch.click(function() {
+		$helpDiv.slideToggle('fast', function() {
+			if ($helpDiv.filter(':visible')) {
+				$.scrollTo($helpSwitch, 500);
+			}
+		});
+		
+		return false;
+	});
+	
+	// Assign classes to special items
+	$('#controls li:last').addClass('last');
+
+});
